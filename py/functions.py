a80a6ab89133404f5fbe9f704c0403f8ddb488ff
KyeWilson <kyewilson.phys@gmail.com>
2025-02-07 08:39:39 +0000

    Imported DecisionTreeClassifier to run Q1

--- py/functions.py
+++ py/functions.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
